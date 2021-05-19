fe207e6f9f28794c705b7270b3c89d000f20a26a
sangyetenphel <sangyetenphel@gmail.com>
2021-05-18 23:18:47 -0400

    Created product model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def  __str__(self):
+        return f"{self.name} - {self.price}"
--- store/views.py
+++ store/views.py
@@ -1,20 +1,10 @@
 from django.shortcuts import render
-
-products = [
-    {
-        'name': 'Electric toothbrush',
-        'price': '$30'
-    },
-    {
-        'name': 'Bamboo toothbrush',
-        'price': '$20'
-    },
-]
+from .models import Product
 
 # Create your views here.
 def home(request):
     context = {
         'title': 'Home',
-        'products': products
+        'products': Product.objects.all()
     }
     return render(request, 'store/home.html', context)
