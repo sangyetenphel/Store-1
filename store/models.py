b92338966b7fb9ebc18f736f679a7c2f92bb9afe
sangyetenphel <sangyetenphel@gmail.com>
2021-05-21 16:10:44 -0400

    User & Profile update, image resize after upload

--- store/models.py
+++ store/models.py
@@ -1,11 +1,23 @@
 from django.db import models
 from django.utils import timezone
+from PIL import Image
 
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     date_added = models.DateTimeField(default=timezone.now)
+    image = models.ImageField(default="product_default.jpg", upload_to="product_pics")
 
     def  __str__(self):
         return f"{self.name} - {self.price}"
+
+    def save(self):
+        super().save()
+         
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,10 @@
+from django.db.models.query_utils import FilteredRelation
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import fields
 from django.forms.fields import EmailField
+from .models import Profile
 
 class UserRegistrationForm(UserCreationForm):
     # Adding email to UserCreationForm besides the default of username and password
@@ -13,3 +15,17 @@ class UserRegistrationForm(UserCreationForm):
         model = User
         # Fields to display in order in front-end
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
