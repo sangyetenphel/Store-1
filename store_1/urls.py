3837cf4c9e4f25737aa841a3e97a32ed8e6e6e0f
sangyetenphel <sangyetenphel@gmail.com>
2021-06-09 02:29:25 -0400

    Bootstrap navbar completed

--- store_1/urls.py
+++ store_1/urls.py
@@ -23,7 +23,7 @@ from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', user_views.register, name='register'),
+    path('register/', user_views.register, name='user-register'),
     path('profile/', user_views.profile, name='profile'),
     path('login', user_views.user_login, name='user-login'),
     path('logout/', user_views.user_logout, name='user-logout'),
